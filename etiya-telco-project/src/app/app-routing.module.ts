965bcdcc196c03d7c3d220adb1864e8fb0d402ea
Merve Ünver <merveunvr123@gmail.com>
2022-08-20 22:41:02 +0300

    ödev-20

--- etiya-telco-project/src/app/app-routing.module.ts
+++ etiya-telco-project/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { ShowcaseComponent } from './shared/pages/showcase/showcase.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'showcase',pathMatch:'full'},
+  {path:'showcase',component:ShowcaseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
